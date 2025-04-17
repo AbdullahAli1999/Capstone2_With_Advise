eeac7ad59b00a72474e80ce24a8ff6ba63a72cfe
AbdullahAli1999 <108661539+AbdullahAli1999@users.noreply.github.com>
2025-04-17 11:35:26 +0300

    Update ControllerAdvise.java

--- src/main/java/com/example/capstone2_with_advise/Advise/ControllerAdvise.java
+++ src/main/java/com/example/capstone2_with_advise/Advise/ControllerAdvise.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.sql.SQLIntegrityConstraintViolationException;
-
+@ControllerAdvice
 public class ControllerAdvise {
     // Our Exception
     @ExceptionHandler(value = ApiException.class)
